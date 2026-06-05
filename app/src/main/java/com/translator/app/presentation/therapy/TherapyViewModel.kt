71b1bf6b488eeeade31ce33d849e2947ed31845c
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-06-05 22:40:31 +0200

    Update file

--- app/src/main/java/com/translator/app/presentation/therapy/TherapyViewModel.kt
+++ app/src/main/java/com/translator/app/presentation/therapy/TherapyViewModel.kt
@@ -1,7 +1,7 @@
 package com.translator.app.presentation.therapy
 
 import android.Manifest
-import android.content.Context // <-- Недостающий импорт, исправляющий ошибку сборки KSP
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.core.content.ContextCompat
